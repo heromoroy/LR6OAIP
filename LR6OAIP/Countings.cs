bad51b5750e80cbebc9907590f1ea14b518ddc84
Veronika Kashina <47078407+heromoroy@users.noreply.github.com>
2020-12-18 16:22:51 +0300

    LR6OAIP

---
+++ LR6OAIP/Countings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR6OAIP
+{
+    class Sphere
+    {
+        private double R;
+
+        public double Area()
+        {
+            return (4 * 3.14 * Math.Pow(R, 2));
+        }
+
+        public double Сircumference()
+        {
+            return ((4 / 3) * 3.14 * Math.Pow(R, 3));
+        }
+
+        public void Information()
+        {
+#if DEBUG
+            Console.WriteLine();
+#endif
+            String str =
+                "***************\n" +
+                "*****     *****\n" +
+                "***         ***\n" +
+                "*     Шар     *\n" +
+                "***         ***\n" +
+                "*****     *****\n" +
+                "***************\n";
+            Console.WriteLine(str);
+            Console.WriteLine("Площадь шара:");
+            Console.WriteLine(string.Format("V = {0:0.000}", Area()));
+            Console.WriteLine("Обьем шара:");
+            Console.WriteLine(string.Format("S = {0:0.000}", Сircumference()));
+        }
+
+        public void Load()
+        {
+            R = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ LR6OAIP/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LR6OAIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 31
+
+#if DEBUG
+            Console.Write("Введите данные для радиуса шара. R: ");
+#endif
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_in = new StreamReader(@"sphere_input.txt");
+            var new_out = new StreamWriter(@"sphere_output.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Sphere s = new Sphere();
+            s.Load();
+            s.Information();
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+            Console.ReadKey();
+        }
+    }
+}
